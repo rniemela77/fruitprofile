864f3554c85da95819291480e8307dc1a70e024c
Robert Niemela <rvniemela@hotmail.com>
2022-05-10 20:09:33 -0700

    refactor: rename views to ItemView and ShopView

---
+++ src/views/ItemView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { computed } from "vue";
+
+import { useRoute } from "vue-router";
+import { useInventoryStore } from "@/stores/inventory";
+
+const route = useRoute();
+const inventoryStore = useInventoryStore();
+
+const item = computed(() => {
+  // Convert param 'id' from string to number
+  const paramId = parseInt(route.params.id);
+  return inventoryStore.getItemById(paramId);
+});
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an item page</h1>
+    <p>{{ item }}</p>
+  </div>
+</template>
+
+<style></style>
---
+++ src/views/ShopView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink } from "vue-router";
+
+import { useInventoryStore } from "@/stores/inventory";
+const inventoryStore = useInventoryStore();
+</script>
+
+<template>
+  <main>
+    <div v-for="item in inventoryStore.items" :key="item.id">
+      <RouterLink :to="`/item/${item.id}`">{{ item }}</RouterLink>
+    </div>
+  </main>
+</template>
